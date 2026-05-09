9ad5303d9d4235c9984910f80c598a4ab0825db8
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-09 19:06:40 +0300

    Tasks 1 and 2 uploaded.
    Built-in classes used.

---
+++ ObjectsAndClasses-Lab/01.RandomizeWords/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsArray = Console.ReadLine()
+                .Split(" ");
+
+            Random random = new Random();
+
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                int randomIndex = random.Next(0, wordsArray.Length);
+                string currentWord = wordsArray[i];
+                wordsArray[i] = wordsArray[randomIndex];
+                wordsArray[randomIndex] = currentWord;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, wordsArray));
+        }
+    }
+}
---
+++ ObjectsAndClasses-Lab/02.BigFactorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace _02.BigFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+
+            for (int i = 2; i <= inputNumber; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
