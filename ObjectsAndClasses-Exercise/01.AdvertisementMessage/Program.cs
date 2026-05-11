8f9c209ae6969df840661df3291edb925ec89a5c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-11 10:12:41 +0300

    Tasks 1 and 2 uploaded.

---
+++ ObjectsAndClasses-Exercise/01.AdvertisementMessage/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.AdvertisementMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            int messagesCount = int.Parse(Console.ReadLine());
+
+            Random random = new();
+
+            for (int i = 0; i < messagesCount; i++)
+            {
+                int phraseIndex = random.Next(0, phrases.Length);
+                int eventIndex = random.Next(0, events.Length);
+                int authorIndex = random.Next(0, authors.Length);
+                int citiesIndex = random.Next(0, cities.Length);
+
+                string randomPhrase = phrases[phraseIndex];
+                string randomEvent = events[eventIndex];
+                string randomAuthor = authors[authorIndex];
+                string randomCity = cities[citiesIndex];
+
+                Console.WriteLine($"{randomPhrase} {randomEvent} {randomAuthor} – {randomCity}.");
+            }
+        }
+    }
+}
---
+++ ObjectsAndClasses-Exercise/02.Articles/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _02.Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] articleTokens = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string title = articleTokens[0];
+            string content = articleTokens[1];
+            string author = articleTokens[2];
+            Article article = new(title, content, author);
+
+            int commandsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < commandsCount; i++)
+            {
+                string[] commandTokens = Console.ReadLine()
+                    .Split(": ", StringSplitOptions.RemoveEmptyEntries);
+                string command = commandTokens[0];
+                string value = commandTokens[1];
+
+                if (command == "Edit")
+                {
+                    article.Edit(value);
+                }
+                else if (command == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(value);
+                }
+                else if (command == "Rename")
+                {
+                    article.Rename(value);
+                }
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
