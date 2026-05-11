595e8501600d6fb55b139a4b35c55ecc6795e86e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-11 10:13:43 +0300

    Tasks 5 and 6 uploaded.

---
+++ ObjectsAndClasses-Exercise/05.TeamworkProjects/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+
+namespace _05.TeamworkProjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int teamsCount = int.Parse(Console.ReadLine());
+
+            List<Team> teams = new();
+
+            for (int i = 0; i < teamsCount; i++)
+            {
+                string[] teamTokens = Console.ReadLine()
+                    .Split("-", StringSplitOptions.RemoveEmptyEntries);
+                string creatorName = teamTokens[0];
+                string teamName = teamTokens[1];
+
+                if (teams.Any(t => t.Name == teamName))
+                {
+                    Console.WriteLine($"Team {teamName} was already created!");
+                    continue;
+                }
+
+                if (teams.Any(t => t.Creator == creatorName))
+                {
+                    Console.WriteLine($"{creatorName} cannot create another team!");
+                    continue;
+                }
+
+                Team team = new(teamName, creatorName);
+                teams.Add(team);
+                Console.WriteLine($"Team {teamName} has been created by {creatorName}!");
+            }
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "end of assignment")
+            {
+                string[] tokens = inputLine
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries);
+                string username = tokens[0];
+                string teamName = tokens[1];
+
+                if (!teams.Any(t => t.Name == teamName))
+                {
+                    Console.WriteLine($"Team {teamName} does not exist!");
+                    inputLine = Console.ReadLine();
+                    continue;
+                }
+
+                Team team = teams.First(t => t.Name == teamName);
+
+                if (teams.Any(t => t.Members.Contains(username)) ||
+                    team.Creator == username)
+                {
+                    Console.WriteLine($"Member {username} cannot join team {teamName}!");
+                    inputLine = Console.ReadLine();
+                    continue;
+                }
+
+                team.Members.Add(username);
+
+                inputLine = Console.ReadLine();
+            }
+
+            List<Team> validTeams = teams
+                .FindAll(t => t.Members.Count > 0)
+                .OrderByDescending(t => t.Members.Count)
+                .ThenBy(t => t.Name)
+                .ToList();
+
+            List<Team> invalidTeams = teams
+                .FindAll(t => t.Members.Count == 0)
+                .OrderBy(t => t.Name)
+                .ToList();
+
+            foreach (Team team in validTeams)
+            {
+                Console.WriteLine(team);
+            }
+
+            Console.WriteLine("Teams to disband:");
+
+            foreach (Team team in invalidTeams)
+            {
+                Console.WriteLine(team.Name);
+            }
+        }
+    }
+
+    class Team
+    {
+        public Team(string teamName, string creator)
+        {
+            Name = teamName;
+            Creator = creator;
+            Members = new List<string>();
+        }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Creator { get; set; } = string.Empty;
+
+        public IList<string> Members { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new();
+            builder.AppendLine(Name);
+            builder.AppendLine($"- {Creator}");
+
+            foreach (string member in Members.OrderBy(m => m))
+            {
+                builder.AppendLine($"-- {member}");
+            }
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ ObjectsAndClasses-Exercise/06.VehicleCatalogue/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+
+namespace _06.VehicleCatalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<IVehicle> vehicles = new();
+
+            while (input != "End")
+            {
+                string[] vehicleTokens = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string type = vehicleTokens[0];
+                string model = vehicleTokens[1];
+                string color = vehicleTokens[2];
+                int horsePower = int.Parse(vehicleTokens[3]);
+
+                IVehicle vehicle = null;
+
+                if (type == "car")
+                {
+                    vehicle = new Car(model, color, horsePower);
+                }
+                else if (type == "truck")
+                {
+                    vehicle = new Truck(model, color, horsePower);
+                }
+
+                vehicles.Add(vehicle);
+
+                input = Console.ReadLine();
+            }
+
+            string vehicleInfo = Console.ReadLine();
+
+            while (vehicleInfo != "Close the Catalogue")
+            {
+                IVehicle vehicle = vehicles.Find(v => v.Model == vehicleInfo);
+
+                if (vehicle != null)
+                {
+                    Console.WriteLine(vehicle);
+                }
+
+                vehicleInfo = Console.ReadLine();
+            }
+
+            List<IVehicle> cars = vehicles.FindAll(v => v.GetType().Name == "Car");
+            List<IVehicle> trucks = vehicles.FindAll(v => v.GetType().Name == "Truck");
+
+            double averageCarHorsePower = 0;
+
+            if (cars.Any())
+            {
+                averageCarHorsePower = cars.Average(c => c.HorsePower);
+            }
+
+            double averageTruckHorsePower = 0;
+
+            if (trucks.Any())
+            {
+                averageTruckHorsePower = trucks.Average(t => t.HorsePower);
+            }
+
+            Console.WriteLine($"Cars have average horsepower of: {averageCarHorsePower:F2}.");
+            Console.WriteLine($"Trucks have average horsepower of: {averageTruckHorsePower:F2}.");
+        }
+    }
+
+    interface IVehicle
+    {
+        string Model { get; set; }
+
+        string Color { get; set; }
+
+        int HorsePower { get; set; }
+    }
+
+    abstract class Vehicle : IVehicle
+    {
+        public Vehicle(string model, string color, int horsePower)
+        {
+            Model = model;
+            Color = color;
+            HorsePower = horsePower;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int HorsePower { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new();
+            builder.AppendLine($"Type: {GetType().Name}");
+            builder.AppendLine($"Model: {Model}");
+            builder.AppendLine($"Color: {Color}");
+            builder.AppendLine($"Horsepower: {HorsePower}");
+            return builder.ToString().TrimEnd();
+        }
+    }
+
+    class Truck : Vehicle
+    {
+        public Truck(string model, string color, int horsePower) :
+            base(model, color, horsePower)
+        {
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public Car(string model, string color, int horsePower) :
+            base(model, color, horsePower)
+        {
+        }
+    }
+}
