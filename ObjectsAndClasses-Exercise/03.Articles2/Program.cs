472c45c561caf9999f50d31c994440d6b80fcbd6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-11 10:13:15 +0300

    Tasks 3 and 4 uploaded.

---
+++ ObjectsAndClasses-Exercise/03.Articles2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _03.Articles2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfArticles = int.Parse(Console.ReadLine());
+
+            Article[] articles = new Article[numberOfArticles];
+
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                string[] articleTokens = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string title = articleTokens[0];
+                string content = articleTokens[1];
+                string author = articleTokens[2];
+                Article article = new(title, content, author);
+                articles[i] = article;
+            }
+
+            foreach (Article article in articles)
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ ObjectsAndClasses-Exercise/04.Students/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _04.Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            Student[] students = new Student[studentsCount];
+            
+            for (int i = 0; i < studentsCount; i++)
+            {
+                string[] studentTokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string firstName = studentTokens[0];
+                string lastName = studentTokens[1];
+                double grade = double.Parse(studentTokens[2]);
+
+                Student student = new(firstName, lastName, grade);
+                students[i] = student;
+            }
+
+            students = students.OrderByDescending(s => s.Grade).ToArray();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:F2}";
+        }
+    }
+}
