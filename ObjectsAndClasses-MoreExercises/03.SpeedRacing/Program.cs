f55637790e376734a1c05c468b71e5a4f94d1589
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-12 14:56:43 +0300

    Tasks 3 and 4 uploaded.

---
+++ ObjectsAndClasses-MoreExercises/03.SpeedRacing/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace _03.SpeedRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] carData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = carData[0];
+                double fuelAmount = double.Parse(carData[1]);
+                double fuelConsumption = double.Parse(carData[2]);
+                Car car = new(model, fuelAmount, fuelConsumption);
+                cars.Add(car);
+            }
+
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "End")
+            {
+                string[] tokens = commandLine
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = tokens[1];
+                double kilometers = double.Parse(tokens[2]);
+                Car carToDrive = cars.FirstOrDefault(c => c.Model == model);
+
+                if (carToDrive != null)
+                {
+                    carToDrive.Drive(kilometers);
+                }
+
+                commandLine = Console.ReadLine();
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public double TraveledDistance { get; set; }
+
+        public void Drive(double kilometers)
+        {
+            double neededAmount = kilometers * FuelConsumption;
+
+            if (neededAmount <= FuelAmount)
+            {
+                FuelAmount -= neededAmount;
+                TraveledDistance += kilometers;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:F2} {TraveledDistance}";
+        }
+    }
+}
---
+++ ObjectsAndClasses-MoreExercises/04.RawData/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace _04.RawData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsCount = int.Parse(Console.ReadLine());
+
+            Car[] cars = new Car[carsCount];
+
+            for (int i = 0; i < carsCount; i++)
+            {
+                string[] carData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = carData[0];
+                int engineSpeed = int.Parse(carData[1]);
+                int enginePower = int.Parse(carData[2]);
+                int cargoWeight = int.Parse(carData[3]);
+                string cargoType = carData[4];
+                Engine engine = new(engineSpeed, enginePower);
+                Cargo cargo = new(cargoWeight, cargoType);
+                Car car = new(model, engine, cargo);
+                cars[i] = car;
+            }
+
+            string command = Console.ReadLine();
+
+            Car[] selectedCars = null;
+
+            if (command == "fragile")
+            {
+                selectedCars = Array
+                    .FindAll(cars, c => c.Cargo.CargoType == "fragile" && c.Cargo.Weight < 1000);
+            }
+            else if (command == "flamable")
+            {
+                selectedCars = Array
+                    .FindAll(cars, c => c.Cargo.CargoType == "flamable" && c.Engine.Power > 250);
+            }
+
+            foreach (Car car in selectedCars)
+            {
+                Console.WriteLine(car.Model);
+            }
+        }
+    }
+
+    public class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+        }
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+    }
+
+    public class Engine
+    {
+        public Engine(int speed, int power)
+        {
+            Speed = speed;
+            Power = power;
+        }
+
+        public int Speed { get; set; }
+
+        public int Power { get; set; }
+    }
+
+    public class Cargo
+    {
+        public Cargo(int weight, string cargoType)
+        {
+            Weight = weight;
+            CargoType = cargoType;
+        }
+
+        public int Weight { get; set; }
+
+        public string CargoType { get; set; }
+    }
+}
