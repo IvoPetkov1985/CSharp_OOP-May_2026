6253bbd779170024987e7b3d21e9e4c2324dff79
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-12 14:56:07 +0300

    Tasks 1 and 2 uploaded.

---
+++ ObjectsAndClasses-MoreExercises/01.CompanyRoster/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+
+namespace _01.CompanyRoster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Department> departments = new();
+
+            int employeesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < employeesCount; i++)
+            {
+                string[] employeeData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string employeeName = employeeData[0];
+                decimal salary = decimal.Parse(employeeData[1]);
+                string departmentName = employeeData[2];
+                Employee employee = new(employeeName, salary);
+                Department department = departments.FirstOrDefault(d => d.Name == departmentName);
+
+                if (department == null)
+                {
+                    department = new(departmentName);
+                    departments.Add(department);
+                }
+
+                department.AddEmployee(employee);
+            }
+
+            decimal bestAverageSalary = 0;
+            Department bestDepartment = null;
+
+            foreach (Department department in departments)
+            {
+                decimal currentSalary = department.GetAverageSalary();
+
+                if (currentSalary > bestAverageSalary)
+                {
+                    bestDepartment = department;
+                    bestAverageSalary = currentSalary;
+                }
+            }
+
+            Console.WriteLine($"Highest Average Salary: {bestDepartment.Name}");
+            Console.WriteLine(bestDepartment.ToString());
+        }
+    }
+
+    public class Employee
+    {
+        public Employee(string name, decimal salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Salary:F2}";
+        }
+    }
+
+    public class Department
+    {
+        public Department(string name)
+        {
+            Name = name;
+            Employees = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+
+        public List<Employee> Employees { get; set; }
+
+        public void AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+
+        public decimal GetAverageSalary()
+        {
+            return Employees.Average(d => d.Salary);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new();
+
+            foreach (Employee emp in Employees.OrderByDescending(e => e.Salary))
+            {
+                builder.AppendLine(emp.ToString());
+            }
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ ObjectsAndClasses-MoreExercises/02.OldestFamilyMember/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _02.OldestFamilyMember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int membersCount = int.Parse(Console.ReadLine());
+            Family family = new();
+
+            for (int i = 0; i < membersCount; i++)
+            {
+                string[] personData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+                Person person = new(name, age);
+                family.AddMember(person);
+            }
+
+            Person searchedPerson = family.GetOldestMember();
+
+            if (searchedPerson != null)
+            {
+                Console.WriteLine(searchedPerson.ToString());
+            }
+
+        }
+    }
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public ICollection<Person> People { get; set; }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldest = People
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+            return oldest;
+        }
+    }
+}
