5e9fd6fba44ccea1abab1981bd8b20b4f70b9f4a
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-13 15:27:17 +0300

    Tasks 3 and 4 uploaded.

---
+++ Inheritance-Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new();
+            int randomIndex = random.Next(0, this.Count);
+            string randomString = this[randomIndex];
+            this.RemoveAt(randomIndex);
+            return randomString;
+        }
+    }
+}
---
+++ Inheritance-Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList strings = new RandomList();
+            strings.Add("1");
+            strings.Add("2");
+            strings.Add("3");
+            strings.Add("4");
+            Console.WriteLine(strings.RandomString());
+        }
+    }
+}
