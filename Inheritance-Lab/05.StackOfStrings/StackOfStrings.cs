b438d2100a19a3381ccecbad45a3056a4c4d9c0e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-13 15:28:11 +0300

    Task 5 uploaded.
    .sln file added.

---
+++ Inheritance-Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,25 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (Count > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> strings)
+        {
+            foreach (string entry in strings)
+            {
+                Push(entry);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ Inheritance-Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings strings = new();
+            strings.AddRange(new string[] { "1914", "May", "24" });
+            Console.WriteLine(string.Join("-", strings));
+            Console.WriteLine(strings.IsEmpty());
+            Console.WriteLine(strings.Count);
+        }
+    }
+}
