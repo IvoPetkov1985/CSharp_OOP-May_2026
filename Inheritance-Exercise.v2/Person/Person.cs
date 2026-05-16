becc75c349c5ba7ea4983cda42dca66075b86710
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-16 14:51:57 +0300

    Tasks 1 and 2 uploaded.

---
+++ Inheritance-Exercise.v2/Person/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} -> Name: {Name}, Age: {Age}";
+        }
+    }
+}
