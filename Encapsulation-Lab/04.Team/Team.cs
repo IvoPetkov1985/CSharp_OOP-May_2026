8019c8794f73c122b4f8a62ab015a8b8956fbffc
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-19 10:51:33 +0300

    Tasks 3 and 4 uploaded.

---
+++ Encapsulation-Lab/04.Team/Team.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get
+            {
+                return firstTeam.AsReadOnly();
+            }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get
+            {
+                return reserveTeam.AsReadOnly();
+            }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new();
+            builder.AppendLine($"First team has {FirstTeam.Count} players.");
+            builder.AppendLine($"Reserve team has {ReserveTeam.Count} players.");
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
