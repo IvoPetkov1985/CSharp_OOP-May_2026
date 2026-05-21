881ad0be6e89d6bbf4e871fa106adbdbdecc317d
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-05-21 13:36:01 +0300

    Two tasks uploaded.

---
+++ Inheritance-Exercise/Restaurant/Cake.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public const double CakeGrams = 250;
+        public const double CakeCalories = 1000;
+        public const decimal CakePrice = 5;
+
+        public Cake(string name)
+            : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+        }
+    }
+}
---
+++ Inheritance-Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,16 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public const double CoffeeMilliliters = 50;
+        public const decimal CoffeePrice = 3.50m;
+
+        public Coffee(string name, double caffeine)
+            : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
